06a64170e7fdbaa1a766a04ced9de80611069242
Frankie Bruno <frankiebruno1991@gmail.com>
2023-05-08 13:18:33 -0500

    first

---
+++ levelupapi/models/__init__.py
@@ -0,0 +1,4 @@
+from .event import Event
+from .game_type import GameType
+from .game import Game
+from .gamer import Gamer
---
+++ levelupapi/models/event.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Event(models.Model):
+    description = models.CharField(max_length=55)
+    date = models.DateField(null=True, blank=True, auto_now=False, auto_now_add=False)
+    time = models.TimeField(auto_now=False, auto_now_add=False)
+    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    attendees = models.ManyToManyField("Gamer", related_name="gamer_events_attending")
